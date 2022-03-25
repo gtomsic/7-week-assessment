f69effa035121262b74618701247b6991d730e49
Gabriel Tomsic <gabriel@awrow.localdomain>
2022-03-25 10:11:02 -0600

    Double append time documentation complete

---
+++ doubleAppend.js
@@ -0,0 +1,37 @@
+/////////////////////////////////
+///////// Double Append /////////
+/////////////////////////////////
+
+// TinyArray
+/* 
+Results for the TinyArray
+insert 768.174458 ms
+append 58.708 μs
+*/
+
+// SmallArray
+/*
+Results for the SmallArray
+insert 767.259958 ms
+append 63.167 μs
+*/
+
+// MediumArray
+/*
+Results for the MediumArray
+insert 767.486833 ms
+append 95.25 μs
+*/
+
+// LargeArray
+/*
+Results for the extraLargeArray
+insert 768.8915 ms
+append 490.583 μs
+*/
+
+// ExtraLarge
+/*
+insert 768.250458 ms
+append 2.27175 ms
+*/
