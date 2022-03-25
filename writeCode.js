a374fd9b4085355792b39ca360b09a044e70671a
Gabriel Tomsic <gabriel@awrow.localdomain>
2022-03-25 11:32:39 -0600

    Pangram sentence done

--- writeCode.js
+++ writeCode.js
@@ -8,3 +8,21 @@ const hasUniqueChars = (text) => {
 }
 
 console.log("HasUnique: ", hasUniqueChars("Moonday"))
+
+// Pangram Sentence
+const str = "I like cats, but not mice"
+const isPangram = (str = "") => {
+  str = str.toLowerCase()
+  const { length } = str
+  const alphabets = "abcdefghijklmnopqrstuvwxyz"
+  const alphaArr = alphabets.split("")
+  for (let i = 0; i < length; i++) {
+    const el = str[i]
+    const index = alphaArr.indexOf(el)
+    if (index !== -1) {
+      alphaArr.splice(index, 1)
+    }
+  }
+  return !alphaArr.length
+}
+console.log(isPangram(str))
